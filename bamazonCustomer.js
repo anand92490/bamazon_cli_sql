f4c692449572e364318986ec49a4f561b9e2a471
Anand Chaudhary <anand92490@gmail.com>
2019-09-03 23:17:12 -0500

    Product table visible on the terminal. Prompt created.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,10 +1,19 @@
 //require the mysql and the inquirer modules
 //create the mysql conncetion
+//Display the product data for the customer
+//prompt the user with the option select the product by ID
+//prompt the user to select the number of units they'd like to purchase
+//check if there is sufficient product to meet the customer's request
+//if not log 'insufficient quantity' and prevent the order form going through
+//if the stock is available process the transaction
+//deduct the quanitiy - User's unit input
+//display the total cost of the customer's purchase
 
-var mysql = require("mysql");
-var inquirer = require("inquirer");
 
-var connection = mysql.createConnection({
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+let connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -14,17 +23,60 @@ var connection = mysql.createConnection({
   
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected to the database as ID " + connection.threadId);
     start();
   });
 
 function start(){
-    connection.query("SELECT item_id, product_name, price FROM products", function(err, res){
+    connection.query("SELECT * FROM products", function(err, response){
         if(err) throw err;
-        var products = res
-        connection.end();
+        console.log("welcome to bamazon.\n");
+
+        for (let i = 0; i < response.length; i++){
+            console.log('ID- ' + response[i].item_id + '| PRODUCT- ' + response[i].product_name + '| DEPARTMENT - ' + response[i].department_name + '| PRICE - ' + response[i].price + '| QUANTITY - ' + response[i].stock_quantity + '\n');
+
+        }
+
+         productSelection();
+
     });
 
-};
+}
 
+
+function  productSelection(){
+inquirer.prompt([
+    {
+     name: "id",
+     type: "input",
+     message: "What is the item ID you would like to purchase?", 
+     validate: function(value){
+         if (isNaN(value) === false){
+             return true;
+
+         }else{
+             console.log("Please enter the product ID to make a selection. \n");
+             return false;
+         }
+     }
+    },
+
+    {
+        name: 'amount',
+        type: 'input',
+        message: "How many units of the product would you like to buy?",
+        validate: function(value){
+            if(isNaN(value) === false){
+                return true;
+            }else{
+                console.log("Please enter the number of units you wish to purchase.\n");
+                return false;
+
+            }
+        }
+
+
+    }
+
+])
+}
 
